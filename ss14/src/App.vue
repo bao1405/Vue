48483a5ff418cf2772e4721217013725d1d21764
bao1405 <lqbbiker15052@gmail.com>
2024-10-03 23:07:42 +0700

    update

---
+++ ss14/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Product/>
+    <ProductDetail/>
+  </div>
+</template>
+
+<script setup>
+import Product from "./components/Product.vue"
+import ProductDetail from "./components/ProductDetail.vue"
+</script>
+
+<style>
+
+</style>
